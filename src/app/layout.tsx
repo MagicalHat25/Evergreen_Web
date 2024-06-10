fa934a44392145c80977597e45ab6c082071e2e4
Dylan Anderson <magiical25@gmail.com>
2024-06-09 22:24:23 -0500

    Got the side select and memory button row in. Need to investigate using rem for the clip path I think, since percentages don't work well when the window size changes.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,26 +17,30 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <nav>
+      <body className={"bg-background"}>
+        <nav className={"m-2"}>
           <div className={"flex justify-between w-full"}>
-            <Button className={"flex-1 mx-0.5"}>
+            <Button className={"flex-1 mx-1 h-14 bg-topbar text-xl font-light"}>
               WELD
             </Button>
-            <Button className={"flex-1 mx-0.5"}>
+            <Button className={"flex-1 mx-1 h-14 bg-topbar text-xl font-light"}>
               TRIGGER
             </Button>
-            <Button className={"flex-1 mx-0.5"}>
+            <Button className={"flex-1 mx-1 h-14 bg-topbar text-xl font-light"}>
               SEQUENCE
             </Button>
-            <Button className={"flex-1 mx-0.5"}>
-              
+            <Button className={"flex-1 mx-1 h-14 bg-topbar text-xl font-light"}>
             </Button>
-            <Button className={"flex-1 mx-0.5"}>
+            <Button className={"flex-1 mx-1 h-14 bg-topbar text-xl font-light"}>
               MORE -{">"}
             </Button>
             </div>
         </nav>
+        <div className={"flex justify-between w-full"}>
+          <div className={"left-arrow-box"}></div>
+          <div className={"memory-box"}></div>
+          <div className={"right-arrow-box"}></div>
+        </div>
       {children}
       </body>
     </html>

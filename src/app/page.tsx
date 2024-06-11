323262cf84de4f6674bc6492833de3815bdb7ca8
Dylan Anderson <magiical25@gmail.com>
2024-06-10 23:09:03 -0500

    Made a change to use some functions to help clean things up. May add some more functions in the future and move these things into bespoke files to better organize stuff.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,45 +1,68 @@
 import Image from 'next/image';
 import FourTIcon from "../../public/svg/4T Icon.svg";
+import React, { ReactNode } from 'react';
 
+interface IconBarProps {
+  children: ReactNode;
+}
+
+function IconBar({ children }: IconBarProps) {
+  return (
+    <div className="icon-bar-box">
+      {children}
+    </div>
+  );
+}
+
+function TriggerIconBox() {
+  return (
+    <div className={"trigger-icon-box"}>
+      <div className={"title-box"}>
+        <span className="title-box-text">TRIGGER</span>
+      </div>
+      <div className="icon-row">
+        <Image className={"icon"}
+           priority
+           src={FourTIcon}
+           alt={"4T Icon"}
+        />
+        <Image className={"icon icon.inactive"}
+           priority
+           src={FourTIcon}
+           alt={"4T Icon"}
+        />
+        <Image className={"icon icon.inactive"}
+           priority
+           src={FourTIcon}
+           alt={"4T Icon"}
+        />
+        <Image className={"icon"}
+           priority
+           src={FourTIcon}
+           alt={"4T Icon"}
+        />
+      </div>
+    </div>
+  )
+}
+
+function SequenceIconBox() {
+  return (
+    <div className={"sequence-icon-box"}>
+      <div className={"title-box"}>
+        <span className="title-box-text">SEQUENCE</span>
+      </div>
+    </div>
+  )
+}
 
 export default async function Home() {
-
   return (
     <main>
-      <div className={"icon-bar-box"}>
-        <div className={"trigger-icon-box"}>
-          <div className={"title-box"}>
-            <span className="title-box-text">TRIGGER</span>
-          </div>
-          <div className="icon-row">
-            <Image className={"icon"}
-              priority
-              src={FourTIcon}
-              alt={"4T Icon"}
-            />
-            <Image className={"icon icon.inactive"}
-               priority
-               src={FourTIcon}
-               alt={"4T Icon"}
-            />
-            <Image className={"icon icon.inactive"}
-               priority
-               src={FourTIcon}
-               alt={"4T Icon"}
-            />
-            <Image className={"icon"}
-               priority
-               src={FourTIcon}
-               alt={"4T Icon"}
-            />            
-          </div>
-        </div>
-        <div className={"sequence-icon-box"}>
-          <div className={"title-box"}>
-            <span className="title-box-text">SEQUENCE</span>
-          </div>
-        </div>
-      </div>
+      <IconBar> 
+        <TriggerIconBox />
+        <SequenceIconBox />
+      </IconBar>
     </main>
   );
 }
